2f1240fe645d4c615aab8f6c400530c2ea54f3dc
aliouswang <wsw622@126.com>
2019-12-10 19:57:37 +0800

    inuit

---
+++ mmkv/src/main/cpp/MMKV.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aliouswang on 2019-12-10.
+//
+
+#include "MMKV.h"
---
+++ mmkv/src/main/cpp/MMKV.h
@@ -0,0 +1,14 @@
+//
+// Created by aliouswang on 2019-12-10.
+//
+
+#ifndef MMKV_LEARN_MMKV_H
+#define MMKV_LEARN_MMKV_H
+
+
+class MMKV {
+
+};
+
+
+#endif //MMKV_LEARN_MMKV_H
